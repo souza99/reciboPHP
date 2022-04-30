cc85666237451d29ff353875108190f1be8cbdbc
souza99 <jpfsouza99@gmail.com>
2022-04-29 22:15:19 -0300

    Criando projeto

---
+++ conta.php
@@ -0,0 +1,8 @@
+
+    <?php
+    $valor1 = $_POST["valor1"];
+    $valor2 = $_POST["valor2"];
+    $result = $valor1 + $valor2;
+
+    echo "O resultado é: ".$result;
+    ?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        $valor1 = 10;
+        $valor2 = 5;
+    echo "Olá Mundo PHP ".($valor1+$valor2);
+    ?>
+    </h1>
+
+
+    <form action="conta.php" method="post">
+        valor1: <input type="number" name="valor1"><br><br>
+        valor2: <input type="number" name="valor2"><br><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
